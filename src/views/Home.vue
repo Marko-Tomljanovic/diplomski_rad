e14da25c8a0f5dc4e841fd94ae5c6ebd2c7b21c7
Marko Tomljanovic <markot@windowslive.com>
2021-05-09 12:35:38 +0200

    Update hoverLinks & Prettier
    + add bootstrap icons in public
    + add Prettier
    + obrisan nepotreban kod
    + izgled buttona za prijave

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,38 +1,41 @@
 <template>
   <div class="home">
     <div class="container">
-  <div class="row">
-    <div class="col">
-      
+      <div class="row">
+        <div class="col"></div>
+        <div class="col-6">
+          <form>
+            <div class="mb-3">
+              <label for="exampleInputEmail1" class="form-label"
+                >Tražiš izvođače ?</label
+              >
+              <input
+                v-model="pojam"
+                type="email"
+                class="form-control"
+                id="search"
+                aria-describedby="SearchCon"
+              />
+            </div>
+            <button type="button" class="btn btn-primary">Pretraži</button>
+          </form>
+        </div>
+        <div class="col"></div>
+      </div>
     </div>
-    <div class="col-6">
-       <form>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Tražiš izvođače ?</label>
-    <input v-model="pojam" type="email" class="form-control" id="search" aria-describedby="SearchCon">
-  </div>
-  <button type="button" class="btn btn-primary">Pretraži</button>  
-</form>
-    </div>
-    <div class="col">
-      
-    </div>
-  </div>
-</div>
 
-
- <br><br> Provjera:  {{ pojam }}
+    <br /><br />
+    Provjera: {{ pojam }}
   </div>
 </template>
 
-
 <script>
 export default {
-  name: 'home',
-  data(){
-    return{
-      pojam:''
-    }
-  }
-}
+  name: "home",
+  data() {
+    return {
+      pojam: "",
+    };
+  },
+};
 </script>
--- src/views/Registracija.vue
+++ src/views/Registracija.vue
@@ -1,106 +1,101 @@
 <template>
   <div class="container">
-  <div class="row">
-    <div class="col">
-     
+    <div class="row">
+      <div class="col"></div>
+      <div class="col-6">
+        <form>
+          <div class="mb-3">
+            Registriraj se!<br /><br />
+            <label for="exampleInputEmail1" class="form-label"
+              >Email adresa</label
+            >
+            <input
+              v-model="email"
+              type="email"
+              class="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+            />
+          </div>
+          <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label"
+              >Lozinka</label
+            >
+            <input
+              v-model="lozinka"
+              type="password"
+              class="form-control"
+              id="exampleInputPassword1"
+            />
+          </div>
+          <div class="mb-3">
+            <label for="exampleInputPassword2" class="form-label"
+              >Ponovljena lozinka</label
+            >
+            <input
+              v-model="plozinka"
+              type="password"
+              class="form-control"
+              id="exampleInputPassword2"
+            />
+          </div>
+          <div class="mb-3 form-check">
+            <input
+              v-model="prihvati"
+              type="checkbox"
+              class="form-check-input"
+              id="Check1"
+            />
+            <label class="form-check-label" for="exampleCheck1"
+              >Prihvaćam uvjete korištenja
+            </label>
+          </div>
+          <button @click="registracija" type="button" class="btn btn-primary">
+            Registriraj se!
+          </button>
+        </form>
+      </div>
+      <div class="col"></div>
     </div>
-    <div class="col-6">
-      <form>
-  <div class="mb-3">Registriraj se!<br><br>
-    <label for="exampleInputEmail1" class="form-label">Email adresa</label>
-    <input 
-    v-model="email" 
-    type="email" 
-    class="form-control" 
-    id="exampleInputEmail1" 
-    aria-describedby="emailHelp"
-    >
   </div>
-  <div class="mb-3">
-    <label for="exampleInputPassword1" class="form-label">Lozinka</label>
-    <input
-     v-model="lozinka" 
-     type="password" 
-     class="form-control" 
-     id="exampleInputPassword1"
-     >
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputPassword2" class="form-label">Ponovljena lozinka</label>
-    <input 
-    v-model="plozinka" 
-    type="password" 
-    class="form-control" 
-    id="exampleInputPassword2"
-    >
-  </div>
-  <div class="mb-3 form-check">
-    <input
-    v-model="prihvati"
-     type="checkbox" 
-     class="form-check-input" 
-     id="Check1"
-     >
-    <label 
-      class="form-check-label" 
-      for="exampleCheck1"
-      >Prihvaćam uvjete korištenja
-    </label>
-  </div>
-  <button 
-  @click="registracija" 
-  type="button" 
-  class="btn btn-primary"
-  >Registriraj se!</button>
-</form>
-    </div>
-    <div class="col">
-      
-    </div>
-  </div>
-</div>
 </template>
 
-
-
 <script>
-import {firebase} from "@/firebase"
+import { firebase } from "@/firebase";
 
 export default {
-  name:"Registracija",
-data(){
-  return{
-    email:'',
-    lozinka:'',
-    plozinka:'',
-    prihvati: false,
-  };
-},
-methods: {
-  registracija(){
-     if(this.lozinka != this.plozinka){ 
-       alert('Lozinke nisu iste!');
-     }
-     else if(this.prihvati == false){
-       alert('Potrebno je prihvatiti uvijete korištenja');
-     }else{
-firebase
-    .auth()
-    .createUserWithEmailAndPassword(this.email, this.lozinka)
-    .then(() => {
-        this.$router.replace({ name: 'Home' });
+  name: "Registracija",
+  data() {
+    return {
+      email: "",
+      lozinka: "",
+      plozinka: "",
+      prihvati: false,
+    };
+  },
+  methods: {
+    registracija() {
+      if (this.lozinka != this.plozinka) {
+        alert("Lozinke nisu iste!");
+      } else if (this.prihvati == false) {
+        alert("Potrebno je prihvatiti uvijete korištenja");
+      } else {
+        firebase
+          .auth()
+          .createUserWithEmailAndPassword(this.email, this.lozinka)
+          .then(() => {
+            this.$router.replace({ name: "Home" });
 
-        console.log('Uspiješna registracija');
-      }).catch(function(error){
-        console.error("Došlo je do greške", error);
-        alert(error)
-      });  
-  }
-}}}
+            console.log("Uspiješna registracija");
+          })
+          .catch(function(error) {
+            console.error("Došlo je do greške", error);
+            alert(error);
+          });
+      }
+    },
+  },
+};
 </script>
 
-
-
-<style>
-
-</style>
+<style></style>

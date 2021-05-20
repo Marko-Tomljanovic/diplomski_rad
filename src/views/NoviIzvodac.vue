d13865c4edbf27f85b8e95785712aff7e35fdada
Marko Tomljanovic <markot@windowslive.com>
2021-05-20 15:35:38 +0200

    Izgled profila
    + dodan link rel= za Bootstrap ikone
    + napravljane izgled profila
    + proslijeđeni potrebni podaci
    + dodani inputu za potrebne podatke
    + funkcije za input tel broja
    + CSS

--- src/views/NoviIzvodac.vue
+++ src/views/NoviIzvodac.vue
@@ -7,6 +7,13 @@
       placeholder="Upiši ime firme"
     ></b-form-input>
 
+    <b-form-input
+      class="col-5 mx-auto mb-3"
+      type="text"
+      v-model="vlasnikFirme"
+      placeholder="Dodaj vlasnika firme"
+    ></b-form-input>
+
     <b-form-select
       class="col-5 mb-3"
       style="margin-left: 29%"
@@ -20,11 +27,45 @@
       v-model="mjesto"
       placeholder="Upiši mjesto"
     ></b-form-input>
+
+    <b-form-input
+      class="col-5 mx-auto mb-3"
+      type="text"
+      v-model="adresa"
+      placeholder="Upiši adresu"
+    ></b-form-input>
+
+    <b-form-input
+      class="col-5 mx-auto mb-3"
+      type="text"
+      v-model="sluzbeniEmail"
+      placeholder="Dodaj službeni e-mail firme"
+    ></b-form-input>
+
+    <b-form-input
+      @input="razdvojiTelefon()"
+      class="col-5 mx-auto mb-3"
+      type="text"
+      v-model="telefon"
+      placeholder="Dodaj kontakt telefon firme"
+    ></b-form-input>
+
     <b-form-input
       class="col-5 mx-auto mb-3"
       type="text"
       v-model="oib"
       placeholder="Upiši OIB"
+    ></b-form-input>
+    <div style="margin-left:300px">
+      Neobavezna polja: društvene mreže
+    </div>
+    <br />
+
+    <b-form-input
+      class="col-5 mx-auto mb-3"
+      type="text"
+      v-model="facebook"
+      placeholder="Dodaj URL facebook profila firme"
     ></b-form-input>
     <b-button type="button" @click="ucitaj()" variant="primary"
       >Učitaj</b-button
@@ -40,9 +81,14 @@ export default {
   data() {
     return {
       imeFirme: "",
+      vlasnikFirme: "",
       mjesto: "",
+      adresa: "",
+      sluzbeniEmail: "",
+      telefon: "",
       oib: "",
       profil: "",
+      facebook: "",
       selected: null,
       zupanija: [
         { text: "Odaberi županiju", disabled: true },
@@ -132,20 +178,30 @@ export default {
       db.collection("firme")
         .add({
           ime: this.imeFirme,
+          vlasnikFirme: this.vlasnikFirme,
           zupanija: this.selected,
           mjesto: this.mjesto,
+          adresa: this.adresa,
+          sluzbeniEmail: this.sluzbeniEmail,
+          telefon: this.telefon,
           oib: this.oib,
-          email: store.trenutniKorisnik,
+          userEmail: store.trenutniKorisnik,
           profil: "/Profil/" + this.imeFirme, // .replace(" ", "", "g"),
           pic: "https://picsum.photos/150/150",
+          facebook: this.facebook,
           vrijemeObjave: Date.now(),
         })
         .then((doc) => {
           console.log("Spremljeno", doc);
           this.imeFirme = "";
+          this.vlasnikFirme = "";
           this.selected = null;
           this.mjesto = "";
+          this.adresa = "";
+          this.sluzbeniEmail = "";
+          this.telefon = "";
           this.oib = "";
+          this.facebook = "";
           alert("Podaci firme su uspiješno uneseni!");
         })
         .catch((e) => {
@@ -153,6 +209,11 @@ export default {
         });
 
       console.log("Podaci firme su učitani");
+    },
+    razdvojiTelefon() {
+      if (this.telefon.length == 3) {
+        this.telefon += "/";
+      }
     },
   },
 };

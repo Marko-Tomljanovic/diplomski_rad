5ee4d141ab9633b6364f9758f0656fe3fb80df17
Marko Tomljanovic <markot@windowslive.com>
2021-08-25 19:43:23 +0200

    Zaboravljena lozinka
    + stil i unos u bazu

--- src/views/NovaLozinka.vue
+++ src/views/NovaLozinka.vue
@@ -7,16 +7,21 @@
           <h4>Postavljanje nove lozinke</h4>
           <br />
           <p>
-            Upiši svoju e-mail adresu i stići će ti link za promjenu lozinke
+            Upišite svoju email adresu koji ste koristili pri registraciji i
+            poslat ćemo vam upute za povrat lozinke
           </p>
           <b-form-input
             v-model="email"
-            placeholder="Tvoj e-mail"
-            required
+            placeholder="e-mail"
             type="email"
+            autocomplete="off"
+            required
+            @change="zabLozinka"
           ></b-form-input
           ><br />
-          <b-button variant="success" @click="zabLozinka">Potvrdi</b-button>
+          <b-button class="col" variant="success" @click="zabLozinka"
+            >ZATRAŽI LOZINKU</b-button
+          >
         </div>
         <div class="col"></div>
       </div>
@@ -42,10 +47,35 @@ export default {
         .sendPasswordResetEmail(this.email)
         .then((result) => {
           console.log(result);
+          this.email = "";
+          this.$swal
+            .fire({
+              icon: "success",
+              title: "Uspješno zaprimljen zahtjev",
+              text: "Upute za promjenu lozinke su poslane na email",
+              allowOutsideClick: false,
+              confirmButtonText: "Povratak na prijavu",
+            })
+            .then((result) => {
+              if (result.isConfirmed) {
+                window.location.replace("/Prijava");
+              }
+            });
         })
         .catch((error) => {
           console.error(error.message);
           this.errorMessage = error.message;
+          this.$swal
+            .fire({
+              icon: "info",
+              text: "Unesena EMAIL adresa nije registrirana",
+              confirmButtonText: "POKUŠAJ PONOVNO",
+            })
+            .then((result) => {
+              if (result.isConfirmed) {
+                window.location.replace("/NovaLozinka");
+              }
+            });
         });
     },
   },

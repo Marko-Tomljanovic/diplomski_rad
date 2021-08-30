e16af46aa6990a8d5055a20e273848e3fcac666e
Marko Tomljanovic <markot@windowslive.com>
2021-08-30 15:28:16 +0200

    Update logo

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
         class="bojaNavibar"
         toggleable="lg"
         type="dark"
-        style="height:110px; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+        style="box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
       position: fixed; width: 100%; z-index:10;"
       >
         <b-navbar-brand
